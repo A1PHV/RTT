1fe053944e97033113f62aeafd7f2512b77ec10d
Korotkov Sergey <89749570+A1PHV@users.noreply.github.com>
2022-11-17 17:24:37 +0300

    Full lemmatization of the text with subsequent tokenization on russian language

--- RTTmain.py
+++ RTTmain.py
@@ -1,29 +1,8 @@
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import stopwords
+from Text_Handler import Text_Handler
 
-#nltk.download('punkt')
-#nltk.download('stopwords')
+text = input()
+TH = Text_Handler(text)
+TH.write()
 
-class Text_Handler:
-    def __init__(self, t):
-        self.text = t
 
-    tokenization = []
 
-    def tokenize(self):
-        st = nltk.sent_tokenize(self.text)
-        for i in st:
-            wt = word_tokenize(i)
-            self.tokenization.append(wt)
-        russian_stopwords = stopwords.words("russian")
-        self.tokenization = [word for word in self.tokenization if word not in russian_stopwords]
-
-    def write(self):
-        for i in self.tokenization:
-            print(i)
-
-vertext = input()
-T = Text_Handler(vertext)
-T.tokenize()
-T.write()
---
+++ Text_Handler.py
@@ -0,0 +1,41 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+
+patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
+stopwords_ru = stopwords.words("russian")
+morph = MorphAnalyzer()
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+class Text_Handler:
+    def __init__(self, t):
+        self.text = t
+
+    tokenization = []
+
+    def tokenize(self, doc):
+        doc = re.sub(patterns, ' ', doc)
+        tokens = []
+        for token in doc.split():
+            if token and token not in stopwords_ru:
+                token = token.strip()
+                token = morph.normal_forms(token)[0]
+
+                tokens.append(token)
+        if len(tokens) > 2:
+            return tokens
+        return None
+
+    def write(self):
+        self.tokenization = self.tokenize(self.text)
+        for i in self.tokenization:
+            print(i)
+
+
+
+
