3c7e891e8f497180270221f2380dbcf5f7f75574
Korotkov Sergey <89749570+A1PHV@users.noreply.github.com>
2022-12-06 17:32:08 +0300

    Combined tokenizer and neural network, added bot, added dataset

--- RTTmain.py
+++ RTTmain.py
@@ -1,8 +1,87 @@
 import Word2Vec
+import telebot
+from telebot import types
+import os
 
-text = input()
-Neuronet = Word2Vec.Neuro(text)
+badwordfile = open("badwords").readlines()
+bot = telebot.TeleBot('5752360265:AAGynQkWK2msj2CTMK3WU0_D0YxRp07bWPw')
+lemm_message = []
+bad_words = []
+koeffter = 0
+Neuronet = Word2Vec.Neuro()
 Neuronet.ML()
 
+def cnt(msg):
+    count = 0
+    for i in msg:
+        try:
+            if Neuronet.ret_similarity(i)[0] in badwordfile:
+                bad_words.append(i[0])
+                count += 1
+        except:
+            count += 0.5
+    return count
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Проверить сообщение на опасность!")
+    btn2 = types.KeyboardButton("Задать вопрос")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, text="Здравствуйте, чем я могу вам помочь?".format(message.from_user), reply_markup=markup)
 
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "Проверить сообщение на опасность!"):
+        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn3 = types.KeyboardButton("Отправить пересланное сообщение")
+        btn4 = types.KeyboardButton("Отправить сообщение")
+        markup1.add(btn3, btn4)
+        bot.send_message(message.chat.id, text="В каком виде будет отправлено сообщение?".format(message.from_user), reply_markup=markup1)
+    elif (message.text == "Задать вопрос"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Какая цель в твоей работе?")
+        btn2 = types.KeyboardButton("Как анализировать текст?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+    elif (message.text == "Какая цель в твоей работе?"):
+        bot.send_message(message.chat.id,  text = "Я высокотехнологичная машина, которую научили понимать опасность в сообщениях! Можете отправить мне файл с текстом или само сообщение в другом разделе, и я подскажу вам, грозит ли опасность.")
+
+    elif (message.text == "Отправить сообщение") or (message.text == "Отправить пересланное сообщение"):
+        bot.send_message(message.chat.id,  text = "Все готово к приему сообщения. Чтобы вернуться обратно, напишите: 'Главное меню'.", reply_markup=types.ReplyKeyboardRemove())
+
+    elif message.text == "Как анализировать текст?":
+        bot.send_message(message.chat.id, text="Все очень просто: вернитсь в главное меню и нажмите на кнопочку 'Проверить сообщение на опасность!', далее выберите нужный вам способ.")
+
+    elif (message.text == "Вернуться в главное меню") or (message.text == "Главное меню") or (message.text == "Гм"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Проверить сообщение на опасность!")
+        button2 = types.KeyboardButton("Задать вопрос")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+
+    else:
+        bot.send_message(message.chat.id, text="Я проанализирую это!")
+        lemm_message = Neuronet.lemmatize(str(message))
+        koeffter = (cnt(lemm_message) / len(message.text)) * 100
+        bot.send_message(message.chat.id, text="В тексте были найдены такие слова как:\n")
+        for word in bad_words:
+            bot.send_message(message.chat.id, text=word)
+        bot.send_message(message.chat.id, text="Процент угрозы или возможности противоправных действий по моему алгоритму таков:\n")
+        if koeffter >= 75:
+            bot.send_message(message.chat.id, text="Сообщение имеет максимальный уровень угрозы. Надеемся, что мы смогли вам помочь и вы сможете быстро отреагировать на опасность.\n"
+                                                   "Продолжите отправлять сообщения или напишите 'Главное меню'")
+        elif koeffter < 75 and koeffter >= 50:
+            bot.send_message(message.chat.id, text="Сообщение имеет средний уровень угрозы. Надеемся, что мы смогли вам помочь и вы сможете быстро отреагировать на опасность.\n"
+                                                   "Продолжите отправлять сообщения или напишите 'Главное меню'")
+        elif koeffter < 50 and koeffter >= 25:
+            bot.send_message(message.chat.id, text="Сообщение имеет низкий уровень угрозы. Надеемся, что мы смогли вам помочь и вы сможете быстро отреагировать на опасность.\n"
+                                                   "Продолжите отправлять сообщения или напишите 'Главное меню'")
+        elif koeffter < 25:
+            bot.send_message(message.chat.id, text="Сообщение имеет безопасный уровень угрозы. Надеемся, что мы смогли вам помочь и вы сможете быстро отреагировать на опасность.\n"
+                                                   "Продолжите отправлять сообщения или напишите 'Главное меню'")
+        koeffter = 0
+
+bot.polling(none_stop=True)
--- Word2Vec.py
+++ Word2Vec.py
@@ -1,19 +1,50 @@
 from gensim.models import Word2Vec
-from Tokenizer import Tokenizer
+#import nltk
+import re
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+import pandas as pd
 
-tokenization = []
+patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
+stopwords_ru = stopwords.words("russian")
+morph = MorphAnalyzer()
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
 
 class Neuro:
-    def __init__(self, t):
-        self.text = t
+    def __init__(self):
+        self.data = []
+        #self.msg_terror = pd.read_csv("terrormsg.csv", header=None)
+        self.words = pd.read_csv("terrormsg.csv", header=None)
+        self.msg_terror = pd.read_csv("extract_flibusta_dialogues.1.txt", delimiter="\t", header=None)
+        self.msg_terror.append(self.words)
+        self.msg_terror = self.msg_terror.dropna().drop_duplicates().values.tolist()
+        self.w2v_model = Word2Vec(min_count=5, window=2, negative=5, alpha=0.03, min_alpha=0.0007, sample=6e-5, sg=1)
 
-    def prepare_text(self):
-        Tokenization = Tokenizer(self.text)
-        self.tokenization = Tokenization.tokenization()
+    def lemmatize(self, doc):
+        doc = re.sub(patterns, ' ', doc)
+        tokens = []
+        for token in doc.split():
+            if token and token not in stopwords_ru:
+                token = token.strip()
+                token = morph.normal_forms(token)[0]
+                tokens.append(token)
+        if len(tokens) > 0:
+            return tokens
+        return "пусто"
 
     def ML(self):
-        self.prepare_text()
-        w2v_model = Word2Vec(min_count=10, window=2, negative=10, alpha=0.03, min_alpha=0.0007, sample=6e-5, sg=1)
-        w2v_model.train(tokenization, total_examples=w2v_model.corpus_count, epochs=30, report_delay=1)
-        w2v_model.init_sims(replace=True)
+        for i in self.msg_terror:
+            if i != '#VALUE!':
+                self.data.append(self.lemmatize(str(i)))
+        self.w2v_model.build_vocab(self.data)
+        self.w2v_model.train(self.data, total_examples=self.w2v_model.corpus_count, epochs=30, report_delay=1)
+        self.w2v_model.init_sims(replace=True)
+        print("Обучение завершено")
+
+    def ret_similarity(self, word):
+        return self.w2v_model.wv.most_similar(positive=word)
 
